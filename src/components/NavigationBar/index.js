9deee69355ec33fb1ccf21102967de024dd6c604
Eti Priya <etipriya@gmail.com>
2021-07-30 22:32:10 +0100

    added event in navigation

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -2,7 +2,7 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 
-const NavigationBar = () => {
+const NavigationBar = event => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
