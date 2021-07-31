9705223aa3a28a76ca8a19a1cc72308a831442b8
otivisan22 <71434012+otivisan22@users.noreply.github.com>
2021-07-31 15:36:55 +0100

    added storybook

--- src/components/DashboardCompany/index.js
+++
@@ -1,3 +0,0 @@
-const DashboardCompany = {};
-
-export default DashboardCompany;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql,
-} from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "https://48p1r2roz4.sse.codesandbox.io",
-  cache: new InMemoryCache(),
-});
+import Navbar from "./components/Navbar";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <Navbar> </Navbar>
+      <h1>Cha-Cha-Charity</h1>
+    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
