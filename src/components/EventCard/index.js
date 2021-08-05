ee983f438f8d4552d20a6ec174d00bd8ab808786
Misha <73886737+misha244@users.noreply.github.com>
2021-08-05 11:34:05 +0100

    editting spacing wip

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -2,30 +2,34 @@ import "./eventcard.css";
 
 const EventCard = (props) => {
   return (
-    <div className="event-card-container">
-      <div>
+    <article className="event-card-container">
+      <div className="img-container">
         <figure>
           <img
+            className="img"
             src="https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hhcml0eXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
             alt="Event"
-            width="600"
-            height="400"
           />
         </figure>
       </div>
       <div className="info-container">
         <h3>Event Name</h3>
 
-        <text>
+        <section className="description">
           Event description - this is an event, bla bla bla! Join the event!
           Event description - this is an event, bla bla bla! Join the event!
           Event description - this is an event, bla bla bla! Join the event!
-        </text>
-        <a href="/signup">
-          <button type="button">Sign Up</button>
-        </a>
+        </section>
+        <section>
+          <a href="/interest">
+            <button type="button">I'm interested</button>
+          </a>
+          <a href="/signup">
+            <button type="button">Sign Up</button>
+          </a>
+        </section>
       </div>
-    </div>
+    </article>
   );
 };
 export default EventCard;
