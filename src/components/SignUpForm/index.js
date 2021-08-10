e10724188840bc3087b5d4b75f8eaadfd1d519c2
otivisan22 <71434012+otivisan22@users.noreply.github.com>
2021-08-10 16:02:15 +0100

    modified the import for signup mutation

--- src/components/SignUpForm/index.js
+++ src/components/SignUpForm/index.js
@@ -5,7 +5,7 @@ import { useHistory } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-import { SIGNUP } from "../mutations";
+import { SIGNUP } from "../../graphql/mutations";
 import ErrorModal from "./ErrorModal";
 
 const SignUpForm = () => {
