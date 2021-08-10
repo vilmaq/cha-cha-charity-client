e77a0698f040fadc92d11120f95ec0ffb32fc259
nazimrasheed <mr.rasheed1991@gmail.com>
2021-08-10 22:08:38 +0100

    map through the cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,15 @@
 import MainContainer from "../components/MainContainer";
-
 import Header from "../components/Header";
-
 const Home = () => {
   return (
     <MainContainer>
-      <Header title="All Cards" />
+      <Header title="All Charity Cards" />
       <div className="d-flex flex-wrap justify-content-center">
-        {data.blogs.map(blog => {
-          return (
-            <BlogCard
-              id={blog.id}
-              key={blog.id}
-              title={blog.title}
-              content={blog.content}
-              firstName={blog.user.firstName}
-              lastName={blog.user.lastName}
-              isMyBlog={state.user && blog.user.id === state.user.id}
-            />
-          );
+        {data.cards.map((card) => {
+          return <CharityCards title={card.title} imageUrl={card.imageUrl} />;
         })}
       </div>
     </MainContainer>
   );
 };
-
 export default Home;
