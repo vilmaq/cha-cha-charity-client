959815fb5dccd59622dde0cd073ad132c6e27aee
Eti Priya <etipriya@gmail.com>
2021-08-10 21:44:32 +0100

    working on charity cards

--- src/components/CharityCard/index.js
+++ src/components/CharityCard/index.js
@@ -1,55 +1,16 @@
-import "./charitycard.css";
+import { Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
-const CharityCard = props => {
+const CharityCard = ({ title, imageUrl }) => {
   return (
-    <div className="charity-card-container">
-      <div className="card">
-        <div>
-          <h3 className="heading">Red cross Charity</h3>
-          <figure>
-            <img
-              src="https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hhcml0eXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
-              alt="charity"
-              width="600"
-              height="400"
-            />
-          </figure>
-        </div>
-        <div className="charity-info-container">
-          <button type="button">More info</button>
-        </div>
-      </div>
-
-      <div>
-        <h3 className="heading">Red cross Charity</h3>
-        <figure>
-          <img
-            src="https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hhcml0eXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
-            alt="charity"
-            width="600"
-            height="400"
-          />
-        </figure>
-      </div>
-      <div className="charity-info-container">
-        <button type="button">More info</button>
-      </div>
-
-      <div>
-        <h3 className="heading">Red cross Charity</h3>
-        <figure>
-          <img
-            src="https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hhcml0eXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
-            alt="charity"
-            width="600"
-            height="400"
-          />
-        </figure>
-      </div>
-      <div className="charity-info-container">
-        <button type="button">More info</button>
-      </div>
-    </div>
+    <Card style={{ width: "18rem" }}>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Img variant="top" src={imageUrl} />
+        <Button variant="primary">{title}</Button>
+      </Card.Body>
+    </Card>
   );
 };
+
 export default CharityCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,28 @@
 import MainContainer from "../components/MainContainer";
 
+import Header from "../components/Header";
+
 const Home = () => {
-  return <MainContainer>Home</MainContainer>;
+  return (
+    <MainContainer>
+      <Header title="All Cards" />
+      <div className="d-flex flex-wrap justify-content-center">
+        {data.blogs.map(blog => {
+          return (
+            <BlogCard
+              id={blog.id}
+              key={blog.id}
+              title={blog.title}
+              content={blog.content}
+              firstName={blog.user.firstName}
+              lastName={blog.user.lastName}
+              isMyBlog={state.user && blog.user.id === state.user.id}
+            />
+          );
+        })}
+      </div>
+    </MainContainer>
+  );
 };
 
 export default Home;
