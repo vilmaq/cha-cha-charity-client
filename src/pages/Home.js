c1a281dc4406107466c269d1015db54218d0ac61
Eti Priya <etipriya@gmail.com>
2021-08-11 14:34:16 +0100

    added usequery  for data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import MainContainer from "../components/MainContainer";
 import Header from "../components/Header";
+import CharityCard from "../components/CharityCard";
+import { useQuery } from "@apollo/client";
+
 const Home = () => {
+  const { data } = useQuery;
   return (
     <MainContainer>
       <Header title="All Charity Cards" />
-      <div className="d-flex flex-wrap justify-content-center">
-        {data.cards.map((card) => {
-          return <CharityCards title={card.title} imageUrl={card.imageUrl} />;
+      <div className="Card">
+        {data.cards.map(card => {
+          return <CharityCard title={card.title} imageUrl={card.imageUrl} />;
         })}
       </div>
     </MainContainer>
