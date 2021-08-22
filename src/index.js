bf35c4f9995b639a3c0774ee14b9ed9d7f291793
surajverma2587 <surajverma2587@gmail.com>
2021-08-22 13:48:55 +0100

    bring back react hook for signup

--- src/index.js
+++ src/index.js
@@ -5,9 +5,4 @@ import App from "./App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
