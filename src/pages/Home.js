280211a6c4e24152de549c3e608ebf7507c27186
surajverma2587 <surajverma2587@gmail.com>
2021-08-22 19:02:23 +0100

    clean up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import { Container } from "@material-ui/core";
 import CategoryCard from "../components/CategoryCard/CategoryCard";
-import CharityCard from "../components/CharityCard";
 import MainContainer from "../components/MainContainer";
 import { eventCategories } from "../data";
 import "./home.css";
