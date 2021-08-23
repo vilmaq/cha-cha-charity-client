6f968766fb7f68f8540a191ed32791d3bcc8ae88
Misha <73886737+misha244@users.noreply.github.com>
2021-08-23 21:40:36 +0100

    renaming searchbar to change casing

---
+++ src/components/SearchBar/foo.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { Toolbar, AppBar, TextField } from "@material-ui/core";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Box";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+
+import SearchIcon from "@material-ui/icons/Search";
+
+import ReactHookFormSelect from "../ReactHookFormSelect";
+import { FolderOutlined } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+  form: {
+    padding: 16,
+  },
+  Container: {
+    display: "flex",
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    marginTop: "5px",
+    marginBottom: "5px",
+  },
+  searchIcon: {
+    alignSelf: "flex-end",
+    marginBottom: "5px",
+  },
+  searchInput: {
+    width: "200px",
+    margin: "5px",
+  },
+}));
+
+const foo = (props) => {
+  const classes = useStyles();
+  let history = useHistory();
+
+  const handleSearchChange = (event) => {};
+
+  const { handleSubmit } = useForm();
+
+  const onSubmit = (formData) => {};
+
+  return (
+    <Container>
+      <form onSubmit={handleSubmit(onSubmit)} className={classes.form}></form>
+      <AppBar position="static">
+        <Toolbar>
+          <div className={classes.searchContainer}>
+            <SearchIcon className={classes.searchIcon} />
+            <TextField
+              className={classes.searchInput}
+              onChange={handleSearchChange}
+              label="Event"
+              variant="standard"
+            />
+          </div>
+          <Box component="div" m={1} className={classes.formTitle}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              disableElevation
+              size="large"
+            >
+              Submit
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <form />
+    </Container>
+  );
+};
+
+export default foo;
