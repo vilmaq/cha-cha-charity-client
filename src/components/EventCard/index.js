eb207f292e058928b9a9fe2a01b8b8674d4beae3
Misha <73886737+misha244@users.noreply.github.com>
2021-08-23 23:26:15 +0100

    add updated card

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -1,7 +1,32 @@
-import calendar from "./pablo-68.png";
-import location from "./pablo-51.png";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardHeader from "@material-ui/core/CardHeader";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import LocationOnRoundedIcon from "@material-ui/icons/LocationOn";
+import EventRoundedIcon from "@material-ui/icons/EventRounded";
+import calendar from "./sign-in.png";
 
 import "./eventcard.css";
+import Link from "@material-ui/core/Link";
+
+const useStyles = makeStyles({
+  root: {
+    width: 405,
+    textAlign: "center",
+    margin: 30,
+  },
+  header: {
+    backgroundColor: "#f9d9eb",
+  },
+  links: {
+    justifyContent: "center",
+  },
+});
 
 const EventCard = ({
   id,
@@ -15,51 +40,49 @@ const EventCard = ({
   organizer,
   imageUrl,
 }) => {
+  const classes = useStyles();
   return (
-    <article className="event-card-container">
-      <div className="img-container">
-        <figure>
-          <img
-            className="img"
-            src="https://images.unsplash.com/photo-1431794062232-2a99a5431c6c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80eUrl"
-            alt="Event"
-          />
-        </figure>
-      </div>
-      <div className="info-container">
-        <section className="name">
-          <h4>{name}</h4>
-        </section>
-
-        <section className="description">{description}</section>
-        <section className="when-where">
-          <div className="details">
-            <img src={calendar} alt="calendar" height="24px" />
-            <text>{day}</text>
-          </div>
-
-          <div className="details">
-            <img src={location} alt="location" height="24px" />
-            <text>{city}</text>
-          </div>
-        </section>
-
-        <section>
-          <div className="buttonContainer">
-            <a href="/signupfor">
-              <button type="button" className="buttons">
-                Sign Up
-              </button>
-            </a>
-            <a href={"/event/" + id}>
-              <button type="button" className="buttons">
-                See more
-              </button>
-            </a>
-          </div>
-        </section>
-      </div>
-    </article>
+    <div>
+      <Card className={classes.root}>
+        <CardMedia
+          component="img"
+          alt="event-image"
+          height="180"
+          image={calendar}
+          title="event-image"
+        />
+        <CardHeader className={classes.header}>
+          <Typography gutterBottom variant="h6" component="h2">
+            <LocationOnRoundedIcon style={{ color: "#f36b7f" }} />
+            <EventRoundedIcon style={{ color: "#9fbfff" }} />
+          </Typography>
+        </CardHeader>
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="h2">
+            {name}
+          </Typography>
+          <Typography gutterBottom variant="subtitle2" component="h4">
+            by {organizer}
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+        <CardActions className={classes.links}>
+          <Link>
+            <Button size="small" color="primary">
+              Sign Up
+            </Button>
+          </Link>
+          <Link href={"/event/" + id}>
+            <Button size="small" color="primary">
+              See More
+            </Button>
+          </Link>
+        </CardActions>
+      </Card>
+    </div>
   );
 };
+
 export default EventCard;
