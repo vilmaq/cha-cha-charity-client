ad751691bdc0a10d13bed47f0bc043051bb56ee9
Misha <73886737+misha244@users.noreply.github.com>
2021-08-23 14:37:26 +0100

    align, move button add participant icon

--- src/pages/SingleEvent.js
+++ src/pages/SingleEvent.js
@@ -1,18 +1,57 @@
+// import { useQuery } from "@apollo/client";
+// import { useUserContext } from "../context/UserProvider";
+// import { EVENT } from "../graphql/queries";
+// import LoaderSpinner from "../components/Loader/LoaderSpinner";
+// import { useState } from "react";
+
 import calendar from "../assets/images/illustrations/elements/pablo-68.png";
 import location from "../assets/images/illustrations/elements/pablo-51.png";
-
+import participant from "../assets/images/illustrations/elements/pablo-telephone.png";
 import "./singleevent.css";
 
 const SingleEvent = () => {
+  // const { state } = useUserConh5();
+  // const { data, loading, error } = useQuery(EVENT, {
+  //   variables: {
+  //     eventId: state.eventId,
+  //   },
+  // });
+
+  // const [search, setSearch] = useState("");
+
+  // if (loading) {
+  //   return <LoaderSpinner />;
+  // }
+
+  // if (error) {
+  //   console.log(error);
+  //   return <div>Error</div>;
+  // }
+  // if (data) {
+  //   const handleData = () => {};
   return (
     <div className="event-container">
-      <section className="header-container">
-        <header className="header-img"></header>
-        <h1 className="header-title">
-          DEREK ZOOLANDER'S CENTER FOR KIDS WHO CAN'T READ GOOD
-        </h1>
+      <section
+        className="header-container"
+        // style={{
+        //   backgroundImage: `url: (../assets/images/headerholder.png)`,
+        // }}
+      >
+        <header className="header-img">
+          <img src={"./headerholder.png"} alt="event"></img>
+        </header>
+        <div className="header-title">
+          <h1>Event Name{}</h1>
+          <a href="/addevent" className="btn-tag">
+            {/* this button will save the event for the user to my events */}
+            <button type="button" className="buttons">
+              Sign Up
+            </button>
+          </a>
+        </div>
+
         <section className="header-info">
-          <div className="top-column">
+          <div className="column">
             <div className="info">
               <img src={calendar} alt="calendar" height="36px" />
               <h5>12 September 2021</h5>
@@ -21,14 +60,11 @@ const SingleEvent = () => {
               <img src={location} alt="location" height="36px" />
               <h5>Birmingham</h5>
             </div>
+            <div className="info">
+              <img src={participant} alt="participant" height="36px" />
+              <h5>Participants</h5>
+            </div>
           </div>
-          {/* <div className="bottom-column">
-            <a href="/addevent">
-              <button type="button" className="buttons">
-                Sign Up
-              </button>
-            </a>
-          </div> */}
         </section>
       </section>
       <section className="event-info">
@@ -49,17 +85,9 @@ const SingleEvent = () => {
         </article>
         <article className="event-sidebar">
           <div className="sidebar-info">
-            <a href="/addevent">
-              <button type="button" className="buttons">
-                Sign Up For
-              </button>
-            </a>
-            <br />
-            <text>by (organizer) </text>
-            <br />
-            <text>specific location</text>
-            <br />
-            <text>specific time</text>
+            <h5>by (organizer) </h5>
+            <h5>specific time</h5>
+            <h5>specific location</h5>
           </div>
         </article>
       </section>
