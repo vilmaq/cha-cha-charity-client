f0bad6d9d3fcc2508a43f28796c3d20d45f29ceb
Misha <73886737+misha244@users.noreply.github.com>
2021-08-24 17:37:33 +0100

    comment bootstrap out

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
