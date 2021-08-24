3a02248c0944292245661c6840f150ee1e46e909
Eti Priya <etipriya@gmail.com>
2021-08-24 23:42:37 +0100

    calling categoryMenu in index file

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -5,7 +5,7 @@ import Nav from "react-bootstrap/Nav";
 import Button from "react-bootstrap/Button";
 import charityLogo from "../../images/charityLogo.png";
 
-import Category_menu from "./Category_Menu";
+import CategoryMenu from "./CategoryMenu";
 
 const NavigationBar = () => {
   return (
@@ -24,16 +24,20 @@ const NavigationBar = () => {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/events">Events</Nav.Link>
+            <Nav.Link href="/events" page="events">
+              Events
+            </Nav.Link>
           </Nav>
-
+          if (page = "events")
+          {
+            <Nav>
+              <CategoryMenu />
+            </Nav>
+          }
           <Nav>
-            <Category_menu />
-
             <Nav.Link href="/login">Login</Nav.Link>
             <Nav.Link href="/signup">Sign Up</Nav.Link>
           </Nav>
-
           <Nav>
             <Button variant="link" className="nav-link">
               Logout
