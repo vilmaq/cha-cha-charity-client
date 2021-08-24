a61c970fa406f9611a155d1eddc02e623f36652f
Eti Priya <etipriya@gmail.com>
2021-08-24 12:38:19 +0100

    changed the Footer styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: 435,
+    margin: 20,
+    backgroundColor: "#f8cf61",
+    padding: 5,
+  },
+  footer: {
+    padding: 3,
+    textAlign: "center",
+    backgroundColor: "black",
+    color: "white",
+  },
+}));
 
 export default function Footer() {
+  const classes = useStyles();
+
   return (
-    <footer>
+    <footer className={classes.footer}>
       <p>Copyright &copy; 2021 All Rights Reserved by Cha-Cha-Charity</p>
     </footer>
   );
