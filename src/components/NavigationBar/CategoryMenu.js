1170b554b56d3a1810ef530f54519f91d2d330f3
surajverma2587 <surajverma2587@gmail.com>
2021-08-26 23:06:50 +0100

    clean up

--- src/components/NavigationBar/CategoryMenu.js
+++ src/components/NavigationBar/CategoryMenu.js
@@ -4,7 +4,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
-import InputLabel from "@material-ui/core/InputLabel";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
