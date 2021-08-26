3e10649415efa00d3a07f9057fe9a44b1a44a083
surajverma2587 <surajverma2587@gmail.com>
2021-08-26 22:58:55 +0100

    style nav bar

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -4,9 +4,9 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Button from "react-bootstrap/Button";
 
-import charityLogo from "../../images/charityLogo.png";
 import { useUserContext } from "../../contexts/UserProvider";
 import CategoryMenu from "./CategoryMenu";
+import charityLogo from "../../images/charityLogo.png";
 
 const NavigationBar = () => {
   const { state, dispatch } = useUserContext();
@@ -26,7 +26,11 @@ const NavigationBar = () => {
     >
       <Container>
         <Navbar.Brand href="/">
-          <img src={charityLogo} alt="logo" className="logo" />
+          <img
+            src={charityLogo}
+            alt="logo"
+            style={{ width: "40px", height: "40px" }}
+          />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -35,26 +39,24 @@ const NavigationBar = () => {
             <Nav.Link href="/events/all" page="events">
               Events
             </Nav.Link>
-          </Nav>
-          <Nav>
-            <CategoryMenu />
-          </Nav>
-          {state.user ? (
-            <Nav>
+
+            {state.user ? (
               <Button
                 variant="link"
                 className="nav-link"
                 onClick={handleLogout}
+                style={{ textAlign: "left" }}
               >
                 Logout
               </Button>
-            </Nav>
-          ) : (
-            <Nav>
-              <Nav.Link href="/login">Login</Nav.Link>
-              <Nav.Link href="/signup">Sign Up</Nav.Link>
-            </Nav>
-          )}
+            ) : (
+              <>
+                <Nav.Link href="/login">Login</Nav.Link>
+                <Nav.Link href="/signup">Sign Up</Nav.Link>
+              </>
+            )}
+          </Nav>
+          <CategoryMenu />
         </Navbar.Collapse>
       </Container>
     </Navbar>
