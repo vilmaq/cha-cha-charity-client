5cf9c5a1f76aee6c8aa8d472376803ba8d0c3b98
Misha <73886737+misha244@users.noreply.github.com>
2021-08-31 19:03:54 +0100

    layout wip

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import { Container } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import CategoryCard from "../components/CategoryCard/CategoryCard";
 import MainContainer from "../components/MainContainer";
@@ -6,26 +7,41 @@ import { eventCategories } from "../data";
 import "./home.css";
 import Typed from "react-typed";
 
+const useStyles = makeStyles((theme) => ({
+  intro: {
+    width: 450,
+    padding: 20,
+    margin: 20,
+    textAlign: "center",
+  },
+  eventHeader: {
+    textAlign: "center",
+  },
+}));
+
 const Home = () => {
+  const classes = useStyles();
   return (
     <div>
       <div className="main">
-        <Typed
-          className="text-typed"
-          strings={[
-            "We only have what we give",
-            "Cha-Cha-Charity, where charity begins!",
-            "Only a life lived for others is a life worthwhile",
-            "No act of kindness, no matter how small, is ever wasted",
-            "We can't help everyone, but everyone can help someone",
-            "It is when you give of yourself that you truly give",
-            "No one need wait a single moment to improve the world",
-          ]}
-          typeSpeed={40}
-          backSpeed={50}
-          loop
-        />
-        <section className="intro">
+        <Typography>
+          <Typed
+            className="text-typed"
+            strings={[
+              "We only have what we give",
+              "Cha-Cha-Charity, where charity begins!",
+              "Only a life lived for others is a life worthwhile",
+              "No act of kindness, no matter how small, is ever wasted",
+              "We can't help everyone, but everyone can help someone",
+              "It is when you give of yourself that you truly give",
+              "No one need wait a single moment to improve the world",
+            ]}
+            typeSpeed={60}
+            backSpeed={30}
+            loop
+          />
+        </Typography>
+        <section className={classes.intro}>
           <Typography variant="h5">Welcome to Cha-Cha-Charity! </Typography>
           <Typography>
             The best way to engage with charitable events, or even host your own
@@ -39,6 +55,9 @@ const Home = () => {
           <Typography variant="h6">Join us in making it happen!</Typography>
         </section>
       </div>
+      <Typography className={classes.eventHeader} variant="h4">
+        Explore some of our events!
+      </Typography>
       <MainContainer>
         {eventCategories.map((eventCategory) => {
           return (
